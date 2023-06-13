c7f6ec84476a4ff4dc3456195758f65c23e273a1
dcatonqlave <dc@onqlave.com>
2023-06-13 15:02:30 +1000

    add more options

---
+++ encryption/options.py
@@ -0,0 +1,24 @@
+class DebugOption:
+    def __init__(self, enable_debug: bool) -> None:
+        self._debug = enable_debug
+    
+    def get_debug_option(self) -> bool:
+        return self._debug if self._debug is not None else False
+    
+    def set_debug_option(self, enable_debug: bool) -> None:
+        self._debug = enable_debug
+
+
+class ArxOption:
+    def __init__(self, arx_url: str) -> None:
+        self._arx_url = self._check_url(arx_url)
+        
+    def _check_url(self,url: str) -> str | None:
+        return url
+
+    def set_arx_url(self,url:str) -> None:
+        self._arx_url = self._check_url(url)
+
+    def get_arx_url(self) -> str:
+        return self._arx_url
+    
