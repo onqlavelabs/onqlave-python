dbdd08694b9a83d0fd201fdecc92e6c4996c1a8f
DC <dc@onqlave.com>
2023-06-13 14:10:51 +1000

    add more basic structure

--- credentials/credentials.py
+++ credentials/credentials.py
@@ -1,5 +1,9 @@
 class Credential:
-    def __init__(self) -> None:
-        self._access_key = None
-        self._signing_key = None
-        self._secret_key = None
+    def __init__(self, access_key: str, signing_key: str, secret_key: str) -> None:
+        self._access_key = self._validate_key(access_key)
+        self._signing_key = self._validate_key(signing_key)
+        self._secret_key = self._validate_key(secret_key)
+
+    def _validate_key(self, key: str) -> str | None:
+        return None if key == "" else key
+            
