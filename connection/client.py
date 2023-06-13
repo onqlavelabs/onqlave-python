961e6d79c1604cfcfce605d831ea7fe3146d0d85
dcatonqlave <dc@onqlave.com>
2023-06-13 15:02:30 +1000

    add more options

--- connection/client.py
+++ connection/client.py
@@ -1,6 +1,17 @@
 class RetrySettings:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, count: int, wait_time: int, max_wait_time: int) -> None:
+        self._count = self._validate_and_get_count_value(count)
+        self._wait_time = self._validate_and_get_count_value(wait_time)
+        self._max_wait_time = self._validate_and_get_count_value(max_wait_time)
+
+    def _validate_and_get_count_value(self, count: int) -> int:
+        return count
+    
+    def _validate_and_get_wait_time_value(self, wait_time: int) -> int:
+        return wait_time
+
+    def _validate_and_get_max_wait_time_value(self, max_wait_time: int) -> int:
+        return max_wait_time
 
 class Client:
 
