cdca60f57823a9f4b2d631e0c22fb9a0ef35ff9a
DC <dc@onqlave.com>
2023-06-21 01:33:13 +1000

    add features on Key Manager

--- onqlave/connection/connection.py
+++ onqlave/connection/connection.py
@@ -1,23 +1,33 @@
-
+from credentials.credentials import Credential
+from client import RetrySettings,Client
+from utils.hasher import Hasher
 class Configuration:
-    def __init__(self) -> None:
-        self._credential = None # Credential type
-        self._retry = None # retry setting
-        self._arx_url = None
-        self._arx_id = None
-
-
-
+    def __init__(
+            self, 
+            credential: Credential,
+            retry_setting: RetrySettings,
+            arx_url: str,
+            arx_id: str
+    ) -> None:
+        self._credential = credential # Credential type
+        self._retry = retry_setting # retry setting
+        self._arx_url = arx_url
+        self._arx_id = arx_id
 
 
 class Connection:
     
-    def __init__(self) -> None:
-        self._client = None
-        self._hasher = None
-        self._logger = None
-        self._configuration = None
+    def __init__(
+        self, 
+        configuration: Configuration,
+        hasher: Hasher,
+        logger: any            
+    ) -> None:
+        self._client = Client(configuration._retry,logger)
+        self._hasher = hasher
+        self._logger = logger
+        self._configuration = configuration
 
-    def post(self) -> None:
-        pass
+    # def post(self) -> None:
+    #     pass
 
