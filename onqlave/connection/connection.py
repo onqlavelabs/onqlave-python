fbb4d1c56dcc1c59c518bdc9a089bea72dcdb8b5
DC <dc@onqlave.com>
2023-06-22 00:03:19 +1000

    remove verbose Configuration class

--- onqlave/connection/connection.py
+++ onqlave/connection/connection.py
@@ -1,19 +1,25 @@
 from credentials.credentials import Credential
-from client import RetrySettings,Client
+from connection.client import RetrySettings,Client
 from utils.hasher import Hasher
+
+
 class Configuration:
+
     def __init__(
             self, 
-            credential: Credential,
-            retry_setting: RetrySettings,
+            credentials: Credential,
+            retry_settings: RetrySettings,
             arx_url: str,
             arx_id: str
     ) -> None:
-        self._credential = credential # Credential type
-        self._retry = retry_setting # retry setting
+        self._credentials = credentials # Credential type
+        self._retry_settings = retry_settings # retry setting
         self._arx_url = arx_url
         self._arx_id = arx_id
 
+    def get_retry_setting(self):
+        return self._retry
+    
 
 class Connection:
     
@@ -22,12 +28,12 @@ class Connection:
         configuration: Configuration,
         hasher: Hasher,
         logger: any            
-    ) -> None:
-        self._client = Client(configuration._retry,logger)
+    ):
+        self._retry_settings = configuration._retry_settings
+        self._client = Client(self._retry_settings,logger)
         self._hasher = hasher
         self._logger = logger
         self._configuration = configuration
 
     # def post(self) -> None:
     #     pass
-
