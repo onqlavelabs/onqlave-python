3a16753d7313e6e8958872be74f82280a8328f0b
DC <dc@onqlave.com>
2023-07-12 01:21:40 +1000

    add comments

--- onqlave/encryption/encrypted_stream_processor.py
+++ onqlave/encryption/encrypted_stream_processor.py
@@ -3,10 +3,31 @@ import io
 from keymanager.onqlave_types.types import AlgorithmDeserialiser
 
 class EncryptedStreamProcessor:
+    """A stream processor for the encrypted stream in the decryption process
+    with two functions including reading the header and reading the packet.    
+    """
     def __init__(self, cipher_stream: io.BytesIO) -> None:
+        """Init an EncryptedStreamProcessor object which takes the cipher
+        stream in the decryption process as its main attribute
+
+        Args:
+            cipher_stream: a standard python stream object
+
+        Returns:
+            Nothing
+        """
         self._cipher_stream = cipher_stream
 
     def read_header(self):
+        """Read the header of the cipher stream into a bytearray with length of 4,
+        then extract the algorithm infromation packed in the header
+
+        Args:
+            Nothing
+
+        Returns:
+            The information of algorithm packed in the header, as an AlgorithmDeserialiser object
+        """
         header_len_buffer = bytearray(4)
         data_len = self._cipher_stream.readinto(header_len_buffer)
 
@@ -31,6 +52,14 @@ class EncryptedStreamProcessor:
         return algorithm
     
     def read_packet(self):
+        """Read the data in the packet of the cipher stream
+
+        Args:
+            Nothing
+
+        Returns:
+            A bytearray contains the data in the packet of the cipher stream
+        """
         packet_len_buffer = bytearray(4)
         # should try catch this command
         data_len = self._cipher_stream.readinto(packet_len_buffer)
@@ -39,7 +68,7 @@ class EncryptedStreamProcessor:
             raise Exception # invalid cipher data
         
         packet_len = int.from_bytes(packet_len_buffer,'big')
-        buffer =bytearray(packet_len)
+        buffer = bytearray(packet_len)
         data_len = self._cipher_stream.readinto(buffer)
 
         if data_len < int(packet_len):
--- onqlave/encryption/plain_stream_processor.py
+++ onqlave/encryption/plain_stream_processor.py
@@ -3,12 +3,31 @@ import struct
 
 from keymanager.onqlave_types.types import AlgorithmSerialiser
 class PlainStreamProcessor:
-    """A class that modeling the stream processor for the encrypt/decrypt process
+    """A stream processor for the plain stream in the encryption process
+    with two function including writing the header and writing the packet.
     """
     def __init__(self,cipher_stream: io.BytesIO) -> None:
+        """Init an PlainStreamProcessor object which takes the cipher stream
+        in the encryption process as its main attribute
+
+        Args:
+            cipher_stream: a standard python stream object
+
+        Returns:
+            Nothing
+        """
         self._cipher_stream = cipher_stream
 
     def write_header(self, algorithm: AlgorithmSerialiser):
+        """Serialise the algorithm information to the header
+
+        Args:
+            algorithm: an AlgorithmSerialiser object contains information
+            about the selected encryption algorithm
+
+        Returns:
+            Nothing
+        """
         header = algorithm.serialise()
         try:
             self._cipher_stream.write(header)
@@ -17,6 +36,14 @@ class PlainStreamProcessor:
         
     
     def write_packet(self, packet: bytearray):
+        """Write the data into the cipher stream
+
+        Args:
+            packet: a bytearray contains data
+
+        Returns:
+            Nothing
+        """
         data_len = struct.pack(">I",len(packet))
         try:
             self._cipher_stream.write(data_len)
