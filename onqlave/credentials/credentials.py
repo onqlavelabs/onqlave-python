5749e956f70dadd6fd91d5fd7380bae743fa99e0
DC <dc@onqlave.com>
2023-07-25 15:56:55 +1000

    add loading configuration from json for credential and arx option

--- onqlave/credentials/credentials.py
+++ onqlave/credentials/credentials.py
@@ -1,7 +1,10 @@
+import json
+import os
+
 class Credential:
     """Modeling the configurations of the credentials used in the initialisation of the encrypt/decrypt operation
     """
-    def __init__(self, access_key: str, signing_key: str, secret_key: str) -> None:
+    def __init__(self, access_key: str="", signing_key: str="", secret_key: str="") -> None:
         """Init the Credential object with three values of access_key, signing_key, secret_key,
         these values are packed in the response from the Onqlave platform
         Args:
@@ -18,4 +21,14 @@ class Credential:
 
     def _validate_key(self, key: str) -> str:
         return None if key == "" else key
+    
+    def load_config_from_json(self, json_file_path: str):
+        if not os.path.exists(json_file_path):
+            raise FileNotFoundError
+        with open(json_file_path,"r") as f:
+            credentials = json.load(f)
+            self._access_key = credentials['access_key']
+            self._signing_key = credentials['server_signing_key']
+            self._secret_key = credentials['server_secret_key']
+
             
